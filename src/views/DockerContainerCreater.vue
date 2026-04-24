b95a05729fa9a5f111415b97080a8ec46ee2aa1f
Wenew Zhang <wenewboy@gmail.com>
2026-04-24 14:46:04 +0800

    :

--- src/views/DockerContainerCreater.vue
+++ src/views/DockerContainerCreater.vue
@@ -164,6 +164,7 @@
             class="flex-1 px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
             :placeholder="$t('dockerContainers.dirName')"
           />
+          :
           <input
             v-model="item.container_path"
             type="text"
